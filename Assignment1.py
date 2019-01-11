144294b248ce39bdfd66518d5aad64a7cf5355d5
lalitdale <46473070+lalitdale@users.noreply.github.com>
2019-01-11 22:27:26 +0530

    First assignment of python program

---
+++ Assignment1.py
@@ -0,0 +1,20 @@
+
+Num1 = int (input ("Enter 1st Number: "))
+Num2 = int (input ("Enter 2nd Number: "))
+
+Add = Num1 + Num2
+print ("Addition of Numbers is : " + str (Add))
+
+Subtract = Num1 - Num2
+print ("Subtraction of Numbers is : " + str (Subtract))
+
+Multiply = Num1 * Num2
+print ("Multiplication of Numbers is : " + str (Multiply))
+
+Div = Num1 / Num2
+print ("Division of Numbers is : " + str (Div))
+
+Expo = pow (Num1, Num2)
+print ("Exponential of Numbers is : " + str (Expo))
+
+
